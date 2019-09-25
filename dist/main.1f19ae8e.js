9062bfd9f2fd7c7a4b4a85d699cbb3c5cb3bc4c3
greg <greg@gwilken.com>
2019-09-24 22:49:23 -0700

    added images

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -70173,77 +70173,74 @@ const axios = require('axios');
 
 const MODEL_URL = './models';
 const MTCNN_OPTIONS = {
-  minFaceSize: 100 // docReady( async () => { 
-  //   const videoEl = document.getElementById('inputVideo');
-  //   const canvas = document.getElementById('overlay');
-  //   const ctx = canvas.getContext('2d');
-  //   const statusDiv = document.getElementById('status')
-  //   // const faceScoreDiv = document.getElementById('face-score')
-  //   // const faceLabelDiv = document.getElementById('face-label')
-  //   await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
-  //   await faceapi.loadFaceLandmarkModel(MODEL_URL)
-  //   await faceapi.loadMtcnnModel(MODEL_URL)  
-  //   await faceapi.loadFaceRecognitionModel(MODEL_URL)
-  //   statusDiv.innerHTML = 'Models loaded.'
-  //   let res = await axios.get('/data.json')
-  //   let labeledFaceDescriptors = await Promise.all(
-  //       res.data.map(async elem => {
-  //         let arr = new Float32Array( Object.values(elem._descriptors[0]) )
-  //         return new faceapi.LabeledFaceDescriptors(elem._label, [arr])
-  //       })
-  //     )
-  //   statusDiv.innerHTML = 'Face Descriptors loaded.'
-  //   navigator.getUserMedia(
-  //     { 
-  //       video: {} 
-  //     },
-  //     stream => videoEl.srcObject = stream,
-  //     err => console.error(err)
-  //   )
-  // const options = new faceapi.MtcnnOptions(MTCNN_OPTIONS)
-  // const maxDescriptorDistance = 0.6
-  // const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance)
-  // statusDiv.innerHTML = 'Face Matcher loaded.'
-  // const detect = async () => {
-  //   statusDiv.innerHTML = 'Detecting...'
-  //   const results = await faceapi.detectAllFaces(videoEl, options).withFaceLandmarks().withFaceDescriptors()
-  //   if (results) {
-  //     results.forEach(result => {      
-  //       // faceScoreDiv.innerHTML = `face detected score: ${result.detection.score}`
-  //       ctx.clearRect(0, 0, 640, 480)
-  //       let dims = faceapi.matchDimensions(videoEl, canvas)
-  //       let faceDescription = faceapi.resizeResults(result, dims)
-  //       const matchResults = faceMatcher.findBestMatch(faceDescription.descriptor)
-  //       const box = faceDescription.detection.box
-  //       let text = matchResults.label
-  //       if (text === 'unknown') {
-  //         text = 'not chad'
-  //       }
-  //       // faceLabelDiv.innerHTML = `best match: ${text}`
-  //       const drawBox = new faceapi.draw.DrawBox(
-  //         box, 
-  //         { 
-  //           label: text, 
-  //           lineWidth: 2, 
-  //           boxColor: 'yellow',
-  //           drawLabelOptions: {
-  //             fontColor: 'black',
-  //             fontSize: 36,
-  //             fontStyle: 'Pacifico',
-  //             padding: 6
-  //           } 
-  //         }
-  //       )
-  //       drawBox.draw(canvas)
-  //     })
-  //   }
-  //   requestAnimationFrame(detect);
-  //  }
-  //   videoEl.onplay = detect
-  //   consoleArt()
-  // });
-
+  minFaceSize: 100
 };
+(0, _docReady.docReady)(async () => {
+  const videoEl = document.getElementById('inputVideo');
+  const canvas = document.getElementById('overlay');
+  const ctx = canvas.getContext('2d');
+  const statusDiv = document.getElementById('status'); // const faceScoreDiv = document.getElementById('face-score')
+  // const faceLabelDiv = document.getElementById('face-label')
+
+  await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
+  await faceapi.loadFaceLandmarkModel(MODEL_URL);
+  await faceapi.loadMtcnnModel(MODEL_URL);
+  await faceapi.loadFaceRecognitionModel(MODEL_URL);
+  statusDiv.innerHTML = 'Models loaded.';
+  let res = await axios.get('/data.json');
+  let labeledFaceDescriptors = await Promise.all(res.data.map(async elem => {
+    let arr = new Float32Array(Object.values(elem._descriptors[0]));
+    return new faceapi.LabeledFaceDescriptors(elem._label, [arr]);
+  }));
+  statusDiv.innerHTML = 'Face Descriptors loaded.';
+  navigator.getUserMedia({
+    video: {}
+  }, stream => videoEl.srcObject = stream, err => console.error(err));
+  const options = new faceapi.MtcnnOptions(MTCNN_OPTIONS);
+  const maxDescriptorDistance = 0.6;
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance);
+  statusDiv.innerHTML = 'Face Matcher loaded.';
+
+  const detect = async () => {
+    statusDiv.innerHTML = 'Detecting...';
+    const results = await faceapi.detectAllFaces(videoEl, options).withFaceLandmarks().withFaceDescriptors();
+
+    if (results) {
+      results.forEach(result => {
+        // faceScoreDiv.innerHTML = `face detected score: ${result.detection.score}`
+        ctx.clearRect(0, 0, 640, 480);
+        let dims = faceapi.matchDimensions(videoEl, canvas);
+        let faceDescription = faceapi.resizeResults(result, dims);
+        const matchResults = faceMatcher.findBestMatch(faceDescription.descriptor);
+        const box = faceDescription.detection.box;
+        let text = matchResults.label;
+
+        if (text === 'unknown') {
+          text = 'not chad';
+        } // faceLabelDiv.innerHTML = `best match: ${text}`
+
+
+        const drawBox = new faceapi.draw.DrawBox(box, {
+          label: text,
+          lineWidth: 2,
+          boxColor: 'yellow',
+          drawLabelOptions: {
+            fontColor: 'black',
+            fontSize: 36,
+            fontStyle: 'Pacifico',
+            padding: 6
+          }
+        });
+        drawBox.draw(canvas);
+      });
+    }
+
+    requestAnimationFrame(detect);
+  };
+
+  videoEl.onplay = detect;
+  (0, _logo.consoleArt)();
+});
 },{"./styles/main.scss":"styles/main.scss","face-api.js":"../node_modules/face-api.js/build/es6/index.js","./utils/docReady":"utils/docReady.js","./scripts/logo":"scripts/logo.js","axios":"../node_modules/axios/index.js"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -70272,7 +70269,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57009" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62838" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/main.js
+++ src/main.js
@@ -9,94 +9,94 @@ const MODEL_URL = './models'
 const MTCNN_OPTIONS = { minFaceSize: 100 }
 
 
-// docReady( async () => { 
-//   const videoEl = document.getElementById('inputVideo');
-//   const canvas = document.getElementById('overlay');
-//   const ctx = canvas.getContext('2d');
+docReady( async () => { 
+  const videoEl = document.getElementById('inputVideo');
+  const canvas = document.getElementById('overlay');
+  const ctx = canvas.getContext('2d');
 
-//   const statusDiv = document.getElementById('status')
-//   // const faceScoreDiv = document.getElementById('face-score')
-//   // const faceLabelDiv = document.getElementById('face-label')
+  const statusDiv = document.getElementById('status')
+  // const faceScoreDiv = document.getElementById('face-score')
+  // const faceLabelDiv = document.getElementById('face-label')
 
-//   await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
-//   await faceapi.loadFaceLandmarkModel(MODEL_URL)
-//   await faceapi.loadMtcnnModel(MODEL_URL)  
-//   await faceapi.loadFaceRecognitionModel(MODEL_URL)
+  await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
+  await faceapi.loadFaceLandmarkModel(MODEL_URL)
+  await faceapi.loadMtcnnModel(MODEL_URL)  
+  await faceapi.loadFaceRecognitionModel(MODEL_URL)
 
-//   statusDiv.innerHTML = 'Models loaded.'
+  statusDiv.innerHTML = 'Models loaded.'
 
 
-//   let res = await axios.get('/data.json')
+  let res = await axios.get('/data.json')
   
-//   let labeledFaceDescriptors = await Promise.all(
-//       res.data.map(async elem => {
-//         let arr = new Float32Array( Object.values(elem._descriptors[0]) )
-//         return new faceapi.LabeledFaceDescriptors(elem._label, [arr])
-//       })
-//     )
+  let labeledFaceDescriptors = await Promise.all(
+      res.data.map(async elem => {
+        let arr = new Float32Array( Object.values(elem._descriptors[0]) )
+        return new faceapi.LabeledFaceDescriptors(elem._label, [arr])
+      })
+    )
 
-//   statusDiv.innerHTML = 'Face Descriptors loaded.'
+  statusDiv.innerHTML = 'Face Descriptors loaded.'
   
-//   navigator.getUserMedia(
-//     { 
-//       video: {} 
-//     },
-//     stream => videoEl.srcObject = stream,
-//     err => console.error(err)
-//   )
+  navigator.getUserMedia(
+    { 
+      video: {} 
+    },
+    stream => videoEl.srcObject = stream,
+    err => console.error(err)
+  )
   
-// const options = new faceapi.MtcnnOptions(MTCNN_OPTIONS)
-// const maxDescriptorDistance = 0.6
-// const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance)
+const options = new faceapi.MtcnnOptions(MTCNN_OPTIONS)
+const maxDescriptorDistance = 0.6
+const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance)
 
-// statusDiv.innerHTML = 'Face Matcher loaded.'
+statusDiv.innerHTML = 'Face Matcher loaded.'
 
-// const detect = async () => {
-//   statusDiv.innerHTML = 'Detecting...'
+const detect = async () => {
+  statusDiv.innerHTML = 'Detecting...'
   
-//   const results = await faceapi.detectAllFaces(videoEl, options).withFaceLandmarks().withFaceDescriptors()
+  const results = await faceapi.detectAllFaces(videoEl, options).withFaceLandmarks().withFaceDescriptors()
 
-//   if (results) {
-//     results.forEach(result => {      
-//       // faceScoreDiv.innerHTML = `face detected score: ${result.detection.score}`
+  if (results) {
+    results.forEach(result => {      
+      // faceScoreDiv.innerHTML = `face detected score: ${result.detection.score}`
       
-//       ctx.clearRect(0, 0, 640, 480)
+      ctx.clearRect(0, 0, 640, 480)
       
-//       let dims = faceapi.matchDimensions(videoEl, canvas)
-//       let faceDescription = faceapi.resizeResults(result, dims)
+      let dims = faceapi.matchDimensions(videoEl, canvas)
+      let faceDescription = faceapi.resizeResults(result, dims)
       
-//       const matchResults = faceMatcher.findBestMatch(faceDescription.descriptor)
-//       const box = faceDescription.detection.box
-//       let text = matchResults.label
+      const matchResults = faceMatcher.findBestMatch(faceDescription.descriptor)
+      const box = faceDescription.detection.box
+      let text = matchResults.label
       
-//       if (text === 'unknown') {
-//         text = 'not chad'
-//       }
+      if (text === 'unknown') {
+        text = 'not chad'
+      }
 
-//       // faceLabelDiv.innerHTML = `best match: ${text}`
+      // faceLabelDiv.innerHTML = `best match: ${text}`
     
-//       const drawBox = new faceapi.draw.DrawBox(
-//         box, 
-//         { 
-//           label: text, 
-//           lineWidth: 2, 
-//           boxColor: 'yellow',
-//           drawLabelOptions: {
-//             fontColor: 'black',
-//             fontSize: 36,
-//             fontStyle: 'Pacifico',
-//             padding: 6
-//           } 
-//         }
-//       )
+      const drawBox = new faceapi.draw.DrawBox(
+        box, 
+        { 
+          label: text, 
+          lineWidth: 2, 
+          boxColor: 'yellow',
+          drawLabelOptions: {
+            fontColor: 'black',
+            fontSize: 36,
+            fontStyle: 'Pacifico',
+            padding: 6
+          } 
+        }
+      )
 
-//       drawBox.draw(canvas)
-//     })
-//   }
-//   requestAnimationFrame(detect);
-//  }
+      drawBox.draw(canvas)
+    })
+  }
+  requestAnimationFrame(detect);
+ }
 
-//   videoEl.onplay = detect
+  videoEl.onplay = detect
 
-//   consoleArt()
-// });
+  consoleArt()
+});
